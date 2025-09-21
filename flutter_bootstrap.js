40f21464634e589f993c618f887b2b7ee199c909
Aryan Chachra <aryanchachra1406@gmail.com>
2025-09-22 00:56:34 +0530

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3509609343"
+    serviceWorkerVersion: "1303351042"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -53,7 +53,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "be775d01971779fe1bf1b6a39f9cc6c4
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "e46c6b2e0c50b4bec9059ea796a0b692",
+"flutter_bootstrap.js": "94c30f8a356dcd800f29de870d3a589a",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
